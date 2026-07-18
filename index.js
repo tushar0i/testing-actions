d74843692640809e2b79d6476099290258e9f739
Tushar Thakur <171576246+tushar0i@users.noreply.github.com>
2026-07-18 20:06:04 +0530

    adding test route for checking ci/cd

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Deployment 1!" });
+  res.json({ message: "Deployment 2!" });
 });
+
+app.get('/test',(req,res)=>{
+    res.json({ message: "Everything is up and running!"})
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
